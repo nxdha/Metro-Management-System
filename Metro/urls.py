e8824d52bb7cb9014ea63a69bd6365f39aefdfd6
nxdha <u2103143@rajagiri.edu.in>
2023-12-19 10:34:01 +0530

    Login Sign Up Page

--- Metro/urls.py
+++ Metro/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home  # Import the view function
+from .views import contact, home, metrologin, metrosignup  # Import the view function
 
 urlpatterns = [
     path('', home, name='home'),  # Map the root URL to the home view
+    path('contact/', contact, name='contact'),
+    path('login/', metrologin, name='metrologin'),
+    path('signup/', metrosignup, name='metrosignup'),
 ]
--- Metro/views.py
+++ Metro/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def metrologin(request):
+    return render(request, 'metrologin.html')
+
+def metrosignup(request):
+    return render(request, 'metrosignup.html')
